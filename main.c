3e5795eb290cdd131c0581ac58d154303041dd0f
Pedro <pedroenxadrista1@gmail.com>
2025-06-28 22:15:37 -0300

    vm

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include "chunk.h"
 #include "debug.h"
 #include <stdio.h>
+#include "vm.h"
 
 int main( int argc, const char* argv[]){
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
@@ -11,6 +13,8 @@ int main( int argc, const char* argv[]){
     writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_RETURN,133);
     disassembleChunk(&chunk, "Test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
 return 0;
