4a10a05bb4abe2e33695de82795b1f80f8b5d658
Pedro <pedroenxadrista1@gmail.com>
2025-06-29 21:28:05 -0300

    basic math operations

--- vm.c
+++ vm.c
@@ -18,6 +18,12 @@ void freeVM(){
 static interpretResult run(){
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define BINARY_OP(op) \
+        do{ \
+            double b = pop(); \
+            double a = pop(); \
+            push(a op b); \
+        } while(false)
 
     for(;;) {
         #ifdef DEBUG_TRACE_EXECUTION
@@ -36,12 +42,21 @@ static interpretResult run(){
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+
+            case OP_NEGATE:
+                push (-pop()); 
+                break;
             case OP_RETURN:
                 return INTERPRET_OK;
         }
     }
     #undef READ_BYTE()
     #undef READ_CONSTANT()
+    #undef BINARY_OP
 }
 
 interpretResult interpret(Chunk* chunk){
