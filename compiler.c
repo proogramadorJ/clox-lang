dfa2e736d2870657ebd0680e27ec8b7f280f519e
Pedro <pedroenxadrista1@gmail.com>
2025-07-20 18:54:55 -0300

    parse unary expression like -b

--- compiler.c
+++ compiler.c
@@ -13,7 +13,26 @@ typedef struct {
     bool panicMode;
 } Parser;
 
+typedef enum{
+    PREC_NOME,
+    PREC_ASSIGNEMNT, // =
+    PREC_OR, // or
+    PREC_AND, // or
+    PREC_EQUALITY, // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM, // + -
+    PREC_FACTOR,  // * /
+    PREC_UNARY, // ! -
+    PREC_CALL, // . ()
+    PREC_PRIMARY
+} Precedence;
+
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk(){
+    return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message){
     if(parser.panicMode) return;
@@ -59,14 +78,76 @@ static void consume(TokenType type, const char* message){
     errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte){
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
 
-void compile(const char* source, Chunk* chunk){
+static void emitBytes(uint8_t byte1, uint8_t byte2){
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn(){
+    emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value){
+    int constant = addConstant(currentChunk(), value);
+    if(constant > UINT8_MAX){
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+    return (uint8_t) constant;
+}
+
+static void emitConstant(Value value){
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void endCompiler(){
+    emitReturn();
+}
+
+static void grouping(){
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number(){
+    double value = strtod(parser.previous.start, NULL);
+    emiteConstant(value);
+}
+
+static void unary(){
+    TokenType operatorType = parser.previous.type;
+
+    //compile operand.
+    parsePrecedence(PREC_UNARY);
+
+    //Emit the operator instruction.
+    switch(operatorType){
+        case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+        default: return; // Unreachable.
+    }
+}
+
+static void parsePrecedence(Precedence precedence){
+
+}
+
+static void expression(){
+    parsePrecedence(PREC_ASSIGNEMNT);
+}
+
+bool compile(const char* source, Chunk* chunk){
     initScanner(source);
+    compilingChunk = chunk;
     parser.hadError = false;
     parser.panicMode = false;
 
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
 }
